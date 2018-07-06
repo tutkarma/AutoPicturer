52b830987a892202fb4e2efc58f6c459ede3ee76
karma <karma.police2013@yandex.ru>
2018-07-06 20:01:12 +0300

    add clean text and write to file

--- parseRSS/parse.py
+++ parseRSS/parse.py
@@ -1,6 +1,7 @@
 from client import FeedlyClient
 from readability import Document
 import requests
+import re
 import config
 
 FEEDLY_REDIRECT_URI = "http://fabreadly.com/auth_callback"
@@ -8,13 +9,24 @@ FEEDLY_CLIENT_ID=config.USER_CONFIG['client_id']
 FEEDLY_CLIENT_SECRET=config.USER_CONFIG['client_secret']
 FEEDLY_STREAM=config.USER_CONFIG['category']
 
-feedly_client = FeedlyClient(token=FEEDLY_CLIENT_SECRET, sandbox=False)
-content = feedly_client.get_feed_content(FEEDLY_CLIENT_SECRET,FEEDLY_STREAM,unreadOnly=True)
-links = []
-for item in content['items']:
-    links.append(item['originId'])
-link = links.pop()
-html = requests.get(link).text
-doc = Document(html)
-s = doc.summary()
-print(s)
+def clean_tags(html):
+    cleanr = re.compile('<.*?>')
+    clean_text = re.sub(cleanr, '', html)
+    return clean_text
+
+
+if __name__ == '__main__':
+    feedly_client = FeedlyClient(token=FEEDLY_CLIENT_SECRET, sandbox=False)
+    content = feedly_client.get_feed_content(FEEDLY_CLIENT_SECRET,FEEDLY_STREAM,unreadOnly=True)
+    links = []
+    for item in content['items']:
+        links.append(item['originId'])
+
+    for i, link in enumerate(links):
+        html = requests.get(link).text
+        doc = Document(html)
+        text = doc.summary()
+        text = clean_tags(text)
+        with open(r'./articles/test{0:02d}'.format(i), 'w', encoding='utf8') as f:
+            text = " ".join(text.split())
+            f.write(text)
