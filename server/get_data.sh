9e628eef8f2513674083f753f38af7830848a6d4
karma <karma.police2013@yandex.ru>
2018-07-10 03:38:49 +0300

    download ALL dataset

--- server/get_data.sh
+++ server/get_data.sh
@@ -2,4 +2,30 @@
 
 mkdir -p data
 cd data
-wget https://storage.googleapis.com/openimages/2018_04/image_ids_and_rotation.csv
+mkdir -p imageIDs
+mkdir -p machineLables
+mkdir -p humanLables
+#wget https://storage.googleapis.com/openimages/2018_04/image_ids_and_rotation.csv
+#wget https://storage.googleapis.com/openimages/2018_04/class-descriptions.csv
+
+#wget https://storage.googleapis.com/openimages/2018_04/test/test-images-with-rotation.csv
+mv test-images-with-rotation.csv imageIDs
+#wget https://storage.googleapis.com/openimages/2018_04/validation/validation-images-with-rotation.csv
+mv validation-images-with-rotation.csv imageIDs
+#wget https://storage.googleapis.com/openimages/2018_04/train/train-images-with-labels-with-rotation.csv
+mv train-images-with-labels-with-rotation.csv imageIDs
+
+#wget https://storage.googleapis.com/openimages/2018_04/test/test-annotations-machine-imagelabels.csv
+mv test-annotations-machine-imagelabels.csv machineLables
+#wget https://storage.googleapis.com/openimages/2018_04/validation/validation-annotations-machine-imagelabels.csv
+mv validation-annotations-machine-imagelabels.csv machineLables
+#wget https://storage.googleapis.com/openimages/2018_04/train/train-annotations-machine-imagelabels.csv
+mv train-annotations-machine-imagelabels.csv machineLables
+
+#wget https://storage.googleapis.com/openimages/2018_04/test/test-annotations-human-imagelabels.csv
+mv test-annotations-human-imagelabels.csv humanLables
+#wget https://storage.googleapis.com/openimages/2018_04/validation/validation-annotations-human-imagelabels.csv
+mv validation-annotations-human-imagelabels.csv humanLables
+#wget https://storage.googleapis.com/openimages/2018_04/train/train-annotations-human-imagelabels.csv
+mv train-annotations-human-imagelabels.csv humanLables
+
